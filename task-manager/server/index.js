755ac1b5aa518009cf5563097e0a3126ba08dfe8
Laxmi Singh <laxmisingh26102003@gmail.com>
2026-05-01 19:39:31 +0530

    fix cors origin

--- task-manager/server/index.js
+++ task-manager/server/index.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: ["https://task-manager-abhi.netlify.app", "http://localhost:3000", "http://localhost:3001"],
+        origin: ["https://super-concha-10a501.netlify.app", "http://localhost:3000", "http://localhost:3001"],
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     })
