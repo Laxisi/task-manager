74dff05a7aec1655832f87c2b75162c190c1fa97
Laxmi Singh <laxmisingh26102003@gmail.com>
2026-05-01 17:39:08 +0530

    first commit

---
+++ task-manager/client/src/redux/slices/authSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit"
+// import { user } from "../../assets/data"
+
+const initialState = {
+    user: localStorage.getItem("userInfo")
+        ? JSON.parse(localStorage.getItem("userInfo"))
+        : null,
+     
+
+    isSidebarOpen: false,
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setCredentials: (state, action) => {
+            state.user = action.payload
+            
+            localStorage.setItem("userInfo", JSON.stringify({
+                user:action.payload}))
+        },
+        logout: (state) => {
+            state.user = null
+            
+            localStorage.removeItem("userInfo")
+        },
+        setOpenSidebar: (state, action) => {
+            state.isSidebarOpen = action.payload
+        },
+    },
+})
+
+export const { setCredentials, logout, setOpenSidebar } = authSlice.actions
+
+export default authSlice.reducer
